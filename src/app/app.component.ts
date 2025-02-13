9a75d84abc56fe8b5e1f258d0187c734b83ca58b
Manoraj <Manoraj.Boothalingam@vca.com>
2025-02-13 17:04:11 +0530

    store added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DesignSystems';
+
+  ngOnInit(): void {
+    const test1 = new Promise((res, rej) => {
+
+    })
+    const test = new Observable((obs) => {
+      obs.next(1);
+      setTimeout(() => {
+        console.log('render');
+      }, 1000);
+      setTimeout(() => {
+        console.log('next render');
+      }, 1500);
+      obs.next(2);
+    });
+    test.subscribe((val) => console.log(val));
+  }
 }
